7f926e313b11cbd1ee5623f89f172ae81f252dc8
ES <32005995+eordanis@users.noreply.github.com>
2022-04-19 14:14:02 -0400

    Update test template setup
    JT needs to update config additional libraries to include boost dir loc

--- Project1/MapReduceTest/test.cpp
+++ Project1/MapReduceTest/test.cpp
@@ -1,6 +1,22 @@
 #include "pch.h"
+#include "../MapReduce/FileManagement.h"
+#include <boost/log/trivial.hpp>
 
-TEST(TestCaseName, TestName) {
+//Demo Tests
+TEST(TemplateTest, TestDemo) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+// unresolved external symbol error?
+// FileManagement Tests
+//TEST(FileManagementTests, TestSetDirectory) {
+//	FileManagement m;
+//  ASSERT_EXIT(m.setInputDirectory(""), testing::ExitedWithCode(1), "FileManagement:validateDirPath:\tPath must not be empty.");
+//	ASSERT_EXIT(m.setInputDirectory("test"), testing::ExitedWithCode(1), "FileManagement:get_all:\tPath provided \"test\" is not a valid directory.");
+//  ASSERT_EXIT(m.setOutputDirectory(""), testing::ExitedWithCode(1), "FileManagement:validateDirPath:\tPath must not be empty.");
+//	ASSERT_EXIT(m.setOutputDirectory("test"), testing::ExitedWithCode(1), "FileManagement:get_all:\tPath provided \"test\" is not a valid directory.");
+//  ASSERT_EXIT(m.seTempDirectory(""), testing::ExitedWithCode(1), "FileManagement:validateDirPath:\tPath must not be empty.");
+//	ASSERT_EXIT(m.seTempDirectory("test"), testing::ExitedWithCode(1), "FileManagement:get_all:\tPath provided \"test\" is not a valid directory.");
+//	
+//}
