37aad2731237c6d5f2832eefc2ec5dc497cdabaf
JT Washington <jaytee.washington@gmail.com>
2022-05-12 13:53:16 -0400

    Builds Successfully

--- Project2/MapDLL/IReduce.h
+++ Project2/MapDLL/IReduce.h
@@ -1,11 +1,10 @@
 #pragma once
 #include <string>
-#include "IMap.h"
 
 class IReduce
 {
 public:
     virtual ~IReduce() { ; }
-    virtual void setInputFileName(std::string) = 0;
-    virtual void setTempFileName(std::string) = 0;
+    //virtual void setInputFileName(std::string) = 0;
+    //virtual void setTempFileName(std::string) = 0;
 };
