9c4aea4bcda276112b3233f1052f2b536dd12067
JT Washington <jaytee.washington@gmail.com>
2022-06-11 08:13:28 -0400

    New Client Module

--- Project4/MapReduce/Header/Socket.h
+++ Project4/MapReduce/Header/Socket.h
@@ -1,1 +1,23 @@
+#ifndef __SOCKET_H_INCLUDED__
+#define __SOCKET_H_INCLUDED__
+
+///////////////////////////////////////////////////////////////////
+//  Socket.h    -  header file for Socket Program				 //
+//                                                               //
+//  Language:     Visual C++ 2022, ver 17.1.3                    //
+//  Application:  MapReduce Project 3                            //
+//  Course:		  CSE687 SP22 - Object Oriented Design           //
+//  Authors:      Stephanie Eordanidis                           //
+//                JT Washington                                  //
+//                Syracuse University                            //
+//                jwashi05@syr.edu                               //
+///////////////////////////////////////////////////////////////////
 #pragma once
+
+class Socket {
+
+public:
+	void InitiateSocket();
+};
+
+#endif
---
+++ Project4/MapReduce/Socket.cpp
@@ -0,0 +1,14 @@
+/* The port number is passed as an argument */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "Header/Socket.h"
+
+void Socket::InitiateSocket() 
+{
+
+}
